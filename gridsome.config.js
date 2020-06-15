33e880e4b73bc9297619643255b7eef80e5989b0
Anthony Dugarte <toonny1998@gmail.com>
2020-06-15 16:57:28 -0400

    Add item with dynamic asset import

--- gridsome.config.js
+++ gridsome.config.js
@@ -3,4 +3,7 @@
 module.exports = {
   siteName: 'A. Dugarte',
   plugins: [{ use: 'gridsome-plugin-tailwindcss' }],
+  chainWebpack: config => {
+    config.resolve.alias.set('@images', '~/assets/img')
+  },
 }
