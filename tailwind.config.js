499b5f7bed217b828a1dc6688c51b3832e312b31
Anthony Dugarte <toonny1998@gmail.com>
2020-09-26 07:50:16 -0400

    Add tailwind purge config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,13 @@
 module.exports = {
-  purge: [],
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      'src/components/**/*.vue',
+      'src/layouts/**/*.vue',
+      'src/pages/**/*.vue',
+      'gridsome.config.js',
+    ],
+  },
   theme: {
     extend: {},
   },
