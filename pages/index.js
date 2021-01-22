8f804d2871d9d4e26e09bb55ee38b3b8b4009d41
Anthony Dugarte <toonny1998@gmail.com>
2021-01-22 08:04:31 -0400

    Update personal img

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,21 @@
+import Image from 'next/image'
+
 export default function Home() {
   return (
     <>
       <main className="flex-1 flex flex-col justify-center space-y-12">
-        <div className="mx-auto mt-12 rounded-full overflow-hidden shadow-md">
+        <div className="mx-auto mt-12 rounded-full overflow-hidden shadow-md w-64 h-64 md:w-80 md:h-80 xl:w-96 xl:h-96">
           <a
             href="mailto:contact@anthonydugarte.dev"
             target="_blank"
             rel="noreferrer"
           >
-            <img
-              className="h-64 w-64"
-              src="https://www.gravatar.com/avatar/dbe7c7ec5e2e6623c3cfe6272e680b5e?s=2048"
+            <Image
+              src="https://www.gravatar.com/avatar/dbe7c7ec5e2e6623c3cfe6272e680b5e?s=1024"
               alt="me"
+              width={1024}
+              height={1024}
+              layout="responsive"
             />
           </a>
         </div>
