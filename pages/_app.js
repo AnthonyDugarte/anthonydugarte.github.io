6f5fb19afa16194cbb03ee073f660bf11bbf6065
Anthony Dugarte <toonny1998@gmail.com>
2021-01-22 06:23:45 -0400

    Configure linter

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
- import 'tailwindcss/tailwind.css'
+import 'tailwindcss/tailwind.css'
 import '../styles/globals.css'
 
+// eslint-disable-next-line react/prop-types
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
