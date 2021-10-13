7d4e64e190c0b4114f40d260612e8e366b937713
Anthony Dugarte <toonny1998@gmail.com>
2021-10-12 21:28:34 -0400

    feat: add jit mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 module.exports = {
+  mode: 'jit',
   purge: ['./pages/**/*.js', './components/**/*.js'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
   variants: {
-    extend: {},
+    extend: {
+      width: ['group-hover'],
+      height: ['group-hover'],
+    },
   },
   plugins: [],
 }
