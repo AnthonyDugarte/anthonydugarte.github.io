5a3b3923d585963234d00d0aeb70f08339cd225f
Anthony Dugarte <toonny1998@gmail.com>
2021-12-10 21:30:34 -0400

    fix: Avoid photo jumps when work history loads

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import CodersRankWorkExperience from '@components/codersrank-work-experience'
 
 export default function Home() {
   return (
-    <main className="flex-1 flex flex-col justify-center space-y-12 pb-4">
+    <main className="flex-1 flex flex-col space-y-12 pb-4">
       <div className="mx-auto mt-12 rounded-full overflow-hidden shadow-md w-64 h-64 md:w-80 md:h-80 xl:w-96 xl:h-96">
         <Link href="mailto:hello@anthonydugarte.dev">
           <a target="_blank" rel="noreferrer">
